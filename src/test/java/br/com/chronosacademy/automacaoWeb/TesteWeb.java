d90d99aa98a708d87067175147684f530788e57a
adamvinicius <adamvinicius@gmail.com>
2021-07-01 21:06:54 -0300

    aula 05 - anotacoes

--- src/test/java/br/com/chronosacademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosacademy/automacaoWeb/TesteWeb.java
@@ -1,6 +1,9 @@
 package br.com.chronosacademy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,24 +13,46 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.assertEquals;
 
 public class TesteWeb {
-    @Test
-    public void primeiroTeste(){
+
+    WebDriver driver;
+
+    @Before
+    public void inicializaTeste(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
         driver.manage().window().maximize();
 
-        driver.get("https://www.chronosacademy.com.br/");
+        driver.get("https://www.chronosacademy.com.br");
+    }
+
+    @Test
+    public void primeiroTeste(){
+
         String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
 
         assertEquals("Porque Tempo É Conhecimento", titulo);
-
-        driver.quit();
-
-
-
-
     }
+
+    @Test
+    public void segundoTeste(){
+        String xpathBotao = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[2]/div/div/a";
+        WebElement btnTitulo = driver.findElement(By.xpath(xpathBotao));
+
+        btnTitulo.click();
+
+        String xpathTitulo = "/html/body/div/div/div/div/section[2]/div/div/div/div/div/section/div/div/div/div/div/div/div/h2";
+        WebElement h2Titulo = driver.findElement(By.xpath(xpathTitulo));
+
+        assertEquals("AUTOMAÇÃO SEM COMPLICAÇÃO WEB 2.0", h2Titulo.getText());
+    }
+
+    @After
+    public void finalizaTeste(){
+        driver.quit();
+    }
+
+
 }
