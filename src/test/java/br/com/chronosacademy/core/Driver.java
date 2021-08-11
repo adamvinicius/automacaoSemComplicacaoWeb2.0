e8e564586b9a21cffbd890c0729af3fdaf544015
adamvinicius <adamvinicius@gmail.com>
2021-08-10 21:08:30 -0300

    aula 16 steps e wait

--- src/test/java/br/com/chronosacademy/core/Driver.java
+++ src/test/java/br/com/chronosacademy/core/Driver.java
@@ -2,13 +2,17 @@ package br.com.chronosacademy.core;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Driver {
     private static WebDriver driver;
+    private static WebDriverWait wait;
 
     public Driver(String navegador){
 
@@ -31,11 +35,20 @@ public class Driver {
                 driver = new InternetExplorerDriver();
                 break;
         }
+        wait = new WebDriverWait(driver, 10);
         driver.manage().window().maximize();
     }
 
     public static WebDriver getDriver() {
         return driver;
+    }
+
+    public static void visibilityOf(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static void invisibilityOf(WebElement element){
+        wait.until(ExpectedConditions.invisibilityOf(element));
     }
 
 
